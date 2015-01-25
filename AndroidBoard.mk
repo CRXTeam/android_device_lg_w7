d96ea37197070f409bb1bc87d33068def18737d9
cr3pt <cr3pt@users.noreply.github.com>
2015-01-25 16:09:13 +0100

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 #----------------------------------------------------------------------
 # extra images
 #----------------------------------------------------------------------
-include build/core/generate_extra_images.mk
+#include build/core/generate_extra_images.mk
 
 # include the non-open-source counterpart to this file
 -include vendor/lge/w7/AndroidBoardVendor.mk
